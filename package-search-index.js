a1d8b4f4a4d1fd8ff34c6cd3fa4bab14f57dbd3f
Nicola Improta <58845854+GoogleHelpMeEverytime@users.noreply.github.com>
2020-08-31 19:07:35 +0200

    Delete package-search-index.js

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"functionCode"},{"l":"functionCode.divisione"},{"l":"functionCode.elementi"},{"l":"functionCode.ricomponi"},{"l":"gui"},{"l":"gui.panels"},{"l":"gui.panels.componenti"},{"l":"gui.panels.opzioni"},{"l":"main"}]
